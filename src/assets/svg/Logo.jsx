3bb8988b87ddf9ea885354714f214eec73acba14
brndalx <113251192+brandalx@users.noreply.github.com>
2023-07-22 02:01:03 +0300

    feat: logo layout

---
+++ src/assets/svg/Logo.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function Logo() {
+  return (
+    <div>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        className='icon icon-tabler icon-tabler-message-2-code'
+        width={24}
+        height={24}
+        viewBox='0 0 24 24'
+        strokeWidth={2}
+        stroke='#0147FF'
+        fill='none'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      >
+        <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+        <path d='M8 9h8' />
+        <path d='M8 13h6' />
+        <path d='M12 21l-1 -1l-2 -2h-3a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v6' />
+        <path d='M20 21l2 -2l-2 -2' />
+        <path d='M17 17l-2 2l2 2' />
+      </svg>
+    </div>
+  );
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Box, Container, chakra } from '@chakra-ui/react';
+import { Box, Container, Text, chakra } from '@chakra-ui/react';
 import theme from '../utils/theme';
+import Logo from '../assets/svg/Logo';
 export default function Navbar() {
   return (
     <Box>
       <Container maxW='1400px'>
         <Box mt='10px' borderRadius='16px' borderWidth='1px' py='20px' px='10px'>
           <chakra.header w='full' py={4}>
-            <Box color='primary.default'>navbar</Box>
+            <Box color='primary.default'>
+              <Box display='flex' alignItems='center'>
+                {' '}
+                <Logo />{' '}
+                <Text ms={2} fontSize='xs' fontWeight='bold' color='primary.default'>
+                  EFS Chat
+                </Text>
+              </Box>
+            </Box>
           </chakra.header>
         </Box>
       </Container>
