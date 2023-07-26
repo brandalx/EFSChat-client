1eeb947618455e4b35b5892e82c56c1523f65fda
brndalx <113251192+brandalx@users.noreply.github.com>
2023-07-26 19:01:54 +0300

    feat: 404 page layout

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,7 +1,60 @@
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
 import React from 'react';
+import Logo from '../assets/svg/Logo';
+import { Link } from 'react-router-dom';
 
 const Page404 = () => {
-  return <div>page404</div>;
+  return (
+    <Box>
+      <Box data-aos='zoom-in' display='flex' justifyContent='center' alignItems='center' height='80vh'>
+        <Box
+          textAlign='center'
+          w='50%' // set width to 50%
+        >
+          <Box>
+            <Box>
+              <Flex alignItems='center' justifyContent='center'>
+                {' '}
+                <Logo />
+                <Text fontSize='sm' fontWeight='extrabold' color='primary.default' ml='1'>
+                  EFS Chat
+                </Text>
+              </Flex>
+            </Box>{' '}
+          </Box>
+
+          <Text fontSize='2xl' fontWeight='extrabold' color='primary.default'>
+            404
+          </Text>
+          <Text fontSize='sm' fontWeight='BOLD' color='neutral.grayDark'>
+            Oops, we didnt find this page :(
+          </Text>
+          <Link to='/'>
+            <Button
+              mt={5}
+              px={5}
+              borderRadius={100}
+              background='primary.default'
+              fontWeight='bold'
+              variant='solid'
+              color='neutral.white'
+              borderWidth='1px'
+              borderColor='neutral.white'
+              _hover={{
+                background: 'neutral.white',
+                color: 'primary.default',
+                borderWidth: '1px',
+                borderColor: 'primary.default'
+              }}
+              py={5}
+            >
+              Back home
+            </Button>
+          </Link>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Page404;
