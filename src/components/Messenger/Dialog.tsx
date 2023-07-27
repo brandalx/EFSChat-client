8f74628922bf8ee37ef4a5b4ea3e30e6c8816d4e
brndalx <113251192+brandalx@users.noreply.github.com>
2023-07-27 13:31:09 +0300

    feat: dialog layout

--- src/components/Messenger/Dialog.tsx
+++ src/components/Messenger/Dialog.tsx
@@ -16,14 +16,18 @@ const Dialog: React.FC = ({ data }) => {
       borderRadius='100px'
     >
       <Box display='flex' alignItems='center' justifyContent='space-between'>
-        <Box>
+        <Box alignItems='center' display='flex'>
           <Avatar size='sm' name={data.nickname} />
+          <Box mx={2}>
+            <Text fontWeight='bold' fontSize='2xs' color='neutral.grayDark'>
+              {data.nickname.length > 30 ? data.nickname.substring(0, 30) + '...' : data.nickname}
+            </Text>
+            <Text fontWeight='regular' fontSize='3xs' color='neutral.gray'>
+              {data.lastmessage.length > 30 ? data.lastmessage.substring(0, 30) + '...' : data.lastmessage}
+            </Text>
+          </Box>
         </Box>
-        <Box>
-          <Text fontWeight='regular' fontSize='3xs' color='neutral.grayDark'>
-            {data.lastmessage.substring(0, 25)}
-          </Text>
-        </Box>
+
         <Box>
           <Text color='neutral.grayLight' fontWeight='regular' fontSize='3xs'>
             {formatTime(Number(data.lastmessageTime))}
